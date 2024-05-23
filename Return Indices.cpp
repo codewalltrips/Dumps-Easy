852eea1cc4507320dc124991df13e61438c50374
Triparna singha <153750421+codewalltrips@users.noreply.github.com>
2024-05-23 20:04:35 +0530

    Update and rename Return Indices to Return Indices.cpp

--- Return Indices.cpp
+++ Return Indices.cpp
@@ -1,4 +1,4 @@
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -31,7 +31,7 @@ Only one valid answer exists. with test cases [2,7,11,15]
 [3,2,4]
 6
 [3,3]
-6
+6*/
 
 #include <vector>
 #include <unordered_map>
